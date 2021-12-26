cc87643cf79332748b3950897f9d5a6012c50d98
hyuntall <hyunchul1998@naver.com>
2021-12-27 01:08:15 +0900

    Create Appbar and BottomAppbar

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -14,8 +14,29 @@ class MyApp extends StatelessWidget {
       //home: Text('안녕')
       //hone: Icon(Icons.star)
       //home: Container( width: 50, height: 50, color: Colors.blue,)
-      home: Center(
-        child: Container( width: 50, height: 50, color: Colors.blue,)
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('상단바'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('안녕!'),
+            Icon(Icons.star),
+            Icon(Icons.star),
+            Icon(Icons.star),
+          ],
+        ),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page),
+          ],
+        ),
       )
     );
   }
